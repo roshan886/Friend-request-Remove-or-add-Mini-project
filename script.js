406b6c1d01c323598fa2ee747670e5a724280651
roshan886 <kumarroshan81271@gmail.com>
2026-03-29 20:06:05 +0530

    Fix color logic for friend status toggle

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ let flag=0;
 btn.addEventListener("click",()=>{
   if(flag==0){
     document.querySelector("h5").textContent="Friends"
-    document.querySelector("h5").style.color="red"
+    document.querySelector("h5").style.color="green"
     btn.innerText="Remove friend"
     flag=1;
 
   }
   else{
     document.querySelector("h5").textContent="stranger";
-    document.querySelector("h5").color="green"
+    document.querySelector("h5").color="red"
     flag=0
     alert("your friend is removed")
   }
